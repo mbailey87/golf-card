2b206a75b425b4c1bb1d94e3a593182ddc21bdc0
MattBailey <mattbaileywork@gmail.com>
2024-03-17 20:28:47 -0600

    moved index.html out of dist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}"],
+  content: ["./*.{html}, ./dist/*.{html,js}"],
   theme: {
     extend: {},
   },
